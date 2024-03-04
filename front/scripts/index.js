1c62faadd31a003247d60005880acee803277e4d
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-04 17:38:15 -0300

    Tarea clase 1 terminada

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,38 @@
-console.log(tempData);
+const sectionPeliculas = document.getElementById('sectionPeliculas')
+
+function createTarget({title, year, director, duration, genre, rate, poster}){
+    //creo la tarjeta
+    const target = document.createElement("div")
+
+    //creo los elementos de la tarjeta
+    const titleTarget = document.createElement("h3")
+    const imgTarget = document.createElement("img")
+    const descripcionTarget = document.createElement("div")
+
+    //asigno los valores a los elementos recien creados
+    titleTarget.innerHTML = `<a href="">${title}</a>`;
+    imgTarget.src = poster
+
+    //descomprimo genero
+    const generos = genre.join(' ')
+    //ahora si asigno valores a la descripcion
+    descripcionTarget.innerHTML = ` <p>Año: ${year}<br>
+    Director: ${director}<br>
+    Duration: ${duration}<br>
+    Género: ${generos}<br>
+    Calificación: ${rate}
+    `
+    //agrego estilos a la tarjeta
+    target.classList.add('target')
+    titleTarget.classList.add('titleTarget')
+    imgTarget.classList.add('imgTarget')
+    descripcionTarget.classList.add('descripcionTarget')
+    //appendeo a los hijos
+    target.append(titleTarget, imgTarget,descripcionTarget)
+
+    return target;
+}
+
+const targetas = tempData.map(createTarget)
+
+targetas.forEach(target => sectionPeliculas.appendChild(target))
