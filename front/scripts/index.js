03dc757585fcf138adb3f629777cabee860d2e13
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-05 15:06:57 -0300

    empezando con bootstrap

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,44 +1,45 @@
-$.get("https://students-api.2.us-1.fl0.io/movies",(data, status)=>{
-    mapearArray(data);
-})
+const createTarget = ({
+  title,
+  year,
+  director,
+  duration,
+  genre,
+  rate,
+  poster,
+}) => {
+  //creo la tarjeta
+  const target = document.createElement("div");
 
+  //creo los elementos de la tarjeta
+  const titleTarget = document.createElement("h3");
+  const imgTarget = document.createElement("img");
+  const descripcionTarget = document.createElement("div");
 
-const createTarget = ({title, year, director, duration, genre, rate, poster}) => {
-    //creo la tarjeta
-    const target = document.createElement("div")
+  //asigno los valores a los elementos recien creados
+  titleTarget.innerHTML = `<a href="">${title}</a>`;
+  imgTarget.src = poster;
 
-    //creo los elementos de la tarjeta
-    const titleTarget = document.createElement("h3")
-    const imgTarget = document.createElement("img")
-    const descripcionTarget = document.createElement("div")
-
-    //asigno los valores a los elementos recien creados
-    titleTarget.innerHTML = `<a href="">${title}</a>`;
-    imgTarget.src = poster
-
-    //descomprimo genero
-    const generos = genre.join(' ')
-    //ahora si asigno valores a la descripcion
-    descripcionTarget.innerHTML = ` <p>Año: ${year}<br>
+  //descomprimo genero
+  const generos = genre.join(", ");
+  //ahora si asigno valores a la descripcion
+  descripcionTarget.innerHTML = `<p>Año: ${year}<br>
     Director: ${director}<br>
     Duration: ${duration}<br>
     Género: ${generos}<br>
     Calificación: ${rate}
-    `
-    //agrego estilos a la tarjeta
-    target.classList.add('target')
-    titleTarget.classList.add('titleTarget')
-    imgTarget.classList.add('imgTarget')
-    descripcionTarget.classList.add('descripcionTarget')
-    //appendeo a los hijos
-    target.append(titleTarget, imgTarget,descripcionTarget)
+    `;
+  //agrego estilos a la tarjeta
+  target.classList.add("target");
+  titleTarget.classList.add("titleTarget");
+  imgTarget.classList.add("imgTarget");
+  descripcionTarget.classList.add("descripcionTarget");
+  //appendeo a los hijos a la tarejeta
+  target.append(titleTarget, imgTarget, descripcionTarget);
+  //selecciono el section de las peliculas y appendeo la tarjeta
+  const sectionPeliculas = document.getElementById("sectionPeliculas");
+  sectionPeliculas.appendChild(target);
+};
 
-    return target;
-}
-const mapearArray = (tempData) =>{
-    const sectionPeliculas = document.getElementById('sectionPeliculas')
-
-    const targetas = tempData.map(createTarget)
-
-    targetas.forEach(target => sectionPeliculas.appendChild(target))
-}
+$.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
+  data.forEach(createTarget);
+});
