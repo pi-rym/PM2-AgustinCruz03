09267823e633f8fbb8022b2d2542f6d7efe1c6d0
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-07 00:28:50 -0300

    probe que tengo que actualizar el build ante un cambio

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -2,3 +2,4 @@ const createTarget = require("./createTarget")
 $.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
   data.forEach(createTarget);
 });
+console.log("estoy probando que tengo que actualizar el bundle")
