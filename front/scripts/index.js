7095f5fa9dde9da8103bbea47bd11785fb238bb9
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-10 15:55:58 -0300

    llamamos a la promesa tanto con async/await como con .then().catch() B)

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -7,13 +7,29 @@ const axios = require("axios")
 // console.log("estoy probando que tengo que actualizar el bundle, Agustin sos un crack")
 
 
-// AHORA VAMOS A USAR PROMESAS PARA OBTENER LOS DATOS O UN ERROR
-const promiseMovies = axios.get("https://students-api.2.us-1.fl0.io/movies")
-promiseMovies.then((data)=>{
-  //estamos dentro de la funcion succes handler
-  //IMPORTANTE: si todo sale bien el parametro data de la funcion tiene dentro una propiedad "data" con la información
-  data.data.forEach(createTarget)
-}).catch((error)=>{
-  //estamos dentro de la funcion error handler
-  console.log(error)
-})
+// // AHORA VAMOS A USAR PROMESAS USANDO SUS METODOS THEN Y CATCH PARA OBTENER LOS DATOS O UN ERROR
+// const promiseMovies = axios.get("https://students-api.2.us-1.fl0.io/movies")
+// promiseMovies.then((data)=>{
+//   //estamos dentro de la funcion succes handler
+//   //IMPORTANTE: si todo sale bien el parametro data de la funcion tiene dentro una propiedad "data" con la información
+//   data.data.forEach(createTarget)
+// }).catch((error)=>{
+//   //estamos dentro de la funcion error handler
+//   console.log(error)
+// })
+
+// AHORA VAMOS A USAR PROMESAS USANDO ASYNC/AWAIT PARA OBTENER LOS DATOS O UN ERROR
+const traerInformacion = async () => {
+  try {
+    //intentamos traer la información sin errores
+  const promesa = await axios.get("https://students-api.2.us-1.fl0.io/movies")
+    promesa.data.forEach(createTarget)
+  }
+  catch(err){
+    //si detectó un error el bloque de arriba viene a este bloqué 
+    console.log(err)
+  }
+}
+
+//Y llamo a la funcion async(sino no se ejecuta)
+traerInformacion();
