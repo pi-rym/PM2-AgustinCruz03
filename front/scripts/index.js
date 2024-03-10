51c97167daa376c106caee487e2d2c54450cd302
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-10 15:47:46 -0300

    promesas asincronas con .then().catch()

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,19 @@
 const createTarget = require("./createTarget")
+const axios = require("axios")
+// ESTA ES NUESTRA FORMA VIEJA ANTES DE USAR AXIOS
+// $.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
+//   data.forEach(createTarget);
+// });
+// console.log("estoy probando que tengo que actualizar el bundle, Agustin sos un crack")
 
-$.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
-  data.forEach(createTarget);
-});
-console.log("estoy probando que tengo que actualizar el bundle, Agustin sos un crack")
+
+// AHORA VAMOS A USAR PROMESAS PARA OBTENER LOS DATOS O UN ERROR
+const promiseMovies = axios.get("https://students-api.2.us-1.fl0.io/movies")
+promiseMovies.then((data)=>{
+  //estamos dentro de la funcion succes handler
+  //IMPORTANTE: si todo sale bien el parametro data de la funcion tiene dentro una propiedad "data" con la información
+  data.data.forEach(createTarget)
+}).catch((error)=>{
+  //estamos dentro de la funcion error handler
+  console.log(error)
+})
