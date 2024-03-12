63b22ba8bb2d47324dffea5b41d2e754c08cdb4d
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-11 21:36:25 -0300

    se cambió el link de las tarjetas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,14 +1,14 @@
 const createTarget = require("./createTarget")
 const axios = require("axios")
 // ESTA ES NUESTRA FORMA VIEJA ANTES DE USAR AXIOS
-// $.get("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
+// $.get("https://students-api.up.railway.app/movies", (data, status) => {
 //   data.forEach(createTarget);
 // });
 // console.log("estoy probando que tengo que actualizar el bundle, Agustin sos un crack")
 
 
 // // AHORA VAMOS A USAR PROMESAS USANDO SUS METODOS THEN Y CATCH PARA OBTENER LOS DATOS O UN ERROR
-// const promiseMovies = axios.get("https://students-api.2.us-1.fl0.io/movies")
+// const promiseMovies = axios.get("https://students-api.up.railway.app/movies")
 // promiseMovies.then((data)=>{
 //   //estamos dentro de la funcion succes handler
 //   //IMPORTANTE: si todo sale bien el parametro data de la funcion tiene dentro una propiedad "data" con la información
@@ -22,7 +22,7 @@ const axios = require("axios")
 const traerInformacion = async () => {
   try {
     //intentamos traer la información sin errores
-  const promesa = await axios.get("https://students-api.2.us-1.fl0.io/movies") //esta funcion espera a ser resuelta, como que ya no es tan asincronica no?
+  const promesa = await axios.get("https://students-api.up.railway.app/movies") //esta funcion espera a ser resuelta, como que ya no es tan asincronica no?
 //   //IMPORTANTE: si todo sale bien la promesa tiene dentro una propiedad "data" con la información en este caso el array de pelis
     promesa.data.forEach(createTarget)
   }
