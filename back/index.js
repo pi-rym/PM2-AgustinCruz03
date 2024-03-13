babedbf30adde387b9f9ceee05db045c0814de69
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-13 11:56:22 -0300

    PORT: 3000

--- back/index.js
+++ back/index.js
@@ -1,5 +1,6 @@
 const app = require("./src/server")
 
-app.listen(3000,()=>{
-    console.log("la aplicación se está escuchando en el puerto 3000")
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log(`la aplicación se está escuchando en el puerto ${PORT}`)
 })
