c61eb0903c9e8166f98bbb1bbea4613d6739f054
AgustinCruz03 <agustincruz0905@gmail.com>
2024-03-15 09:37:51 -0300

    manejdor catch en el index

--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 const app = require("./src/server")
 const dbCon = require("./src/config/dbCon")
 
+const PORT = 3000
+
 dbCon().then(res => {
-    const PORT = 3000
+    
     app.listen(PORT,()=>{
         console.log(`la aplicación se está escuchando en el puerto ${PORT}`)
     })
--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 
 const dbCon = async () => {
+    try {
     await mongoose.connect("mongodb+srv://agustincruz0905:odh6qvy9mpl1O7UL@movies.kpvofjd.mongodb.net/Movies")
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = dbCon
